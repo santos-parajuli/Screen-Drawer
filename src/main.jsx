d08218fc56839e7bb9bdac0038ecb71ab9f7c8ca
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2024-12-06 13:25:40 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		{* ThemeProvider is used to manage the dark and light mode throughout the website *}
+		{/* ThemeProvider is used to manage the dark and light mode throughout the website */}
 		<ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
-			{* Since this is a mini app, i choose Context to store data through out the app so that data can be shared among the components *}
+			{/* Since this is a mini app, i choose Context to store data through out the app so that data can be shared among the components */}
 			<AppContextProvider>
 				<App />
 			</AppContextProvider>
