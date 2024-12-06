f2f57689422af93592b974b9d0c4bb7f0af16897
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2024-12-06 13:27:39 -0500

    Update DownloadButton.jsx

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,12 +1,10 @@
 import { Button } from './ui/button';
 import { Canvg } from 'canvg';
 import jsPDF from 'jspdf';
-// DownloadButton.js
 import { useAppContext } from './AppContext';
 
 const DownloadButton = () => {
 	const { svgRef, selectedScreen, selectedReceptacleBox, floorHeight, orientation, niche, nicheGap } = useAppContext();
-
 	const downloadPDF = async () => {
 		const svgElement = svgRef.current;
 		if (!svgElement) {
@@ -21,32 +19,23 @@ const DownloadButton = () => {
 		canvas.width = 1000;
 		canvas.height = 1000;
 		const ctx = canvas.getContext('2d');
-
 		// Use canvg to render the SVG on the canvas
 		await (await Canvg.from(ctx, svgString)).start();
-
 		// Convert the canvas to an image (PNG)
 		const imgData = canvas.toDataURL('image/png');
-
 		// Add the image to the PDF
 		const pdf = new jsPDF();
 		pdf.addImage(imgData, 'PNG', 10, 10, 150, 150); // Adjust dimensions as needed
-
 		// Add AppContext data to the PDF
 		const data = [
-			`Selected Screen: ${JSON.stringify(selectedScreen, null, 2)}`,
-			`Selected Receptacle Box: ${JSON.stringify(selectedReceptacleBox, null, 2)}`,
 			`Floor Height: ${floorHeight}"`,
 			`Orientation: ${orientation}`,
-			`Niche: ${niche}`,
 			`Niche Gap: ${nicheGap}"`,
 		];
-
 		pdf.setFontSize(12);
 		data.forEach((line, i) => {
 			pdf.text(line, 10, 170 + i * 10);
 		});
-
 		// Save the PDF
 		pdf.save('diagram.pdf');
 	};
