6af6a5b53a004b72319c5973096be0540b3a529c
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2024-12-06 00:05:05 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './ui/mode-toggle';
 function Navbar() {
 	return (
 		<div className='flex items-center justify-between px-10 py-3 bg-gray-800 dark:bg-black '>
-			<img src='./public/logo.png' className='h-[50px]' />
+			<img src='/logo.png' className='h-[50px]' />
 			<ModeToggle />
 		</div>
 	);
